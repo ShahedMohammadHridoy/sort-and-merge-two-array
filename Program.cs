68ca8d88d6fb13d4f876b7075e3f3fb1ec3efc77
Shahed <shahed_hridoy@outlook.com>
2022-02-09 00:52:56 +0600

    bubble sort added

--- Program.cs
+++ Program.cs
@@ -17,17 +17,23 @@ namespace Program
 
             int[] tempInt = new int[amount.Length];
 
-            
-
              for(int i=0; i<amount.Length; i++){
                  tempInt[i] = amount[i];
              }
             
-            Array.Sort(amount);
-            Array.Reverse(amount);
+            //sorting
+            int temp;
+            for (int j = 0; j <= amount.Length - 2; j++) {
+            for (int i = 0; i <= amount.Length - 2; i++) {
+               if (amount[i] < amount[i + 1]) {
+                  temp= amount[i + 1];
+                  amount[i + 1] = amount[i];
+                  amount[i] = temp;
+               }
+            }
+         }
 
             for(int i=0; i<amount.Length; i++){
-
                 Console.Write(amount[i] + " - ");
                 Console.WriteLine(name[Array.IndexOf(tempInt,amount[i])]);
             }
