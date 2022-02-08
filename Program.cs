0ab1e8f0003e7aa0b61bbeb8cf32e144756e3b4a
Shahed <shahed_hridoy@outlook.com>
2022-02-09 01:40:54 +0600

    update

--- Program.cs
+++ Program.cs
@@ -16,22 +16,22 @@ namespace Program
             string[] name = {"A","B","C","D","E","F"};
 
             int[] tempInt = new int[amount.Length];
-
-             for(int i=0; i<amount.Length; i++){
-                 tempInt[i] = amount[i];
-             }
+            
+            for(int i=0; i<amount.Length; i++){
+                tempInt[i] = amount[i];
+            }
             
             //sorting
             int temp;
             for (int j = 0; j <= amount.Length - 2; j++) {
-            for (int i = 0; i <= amount.Length - 2; i++) {
-               if (amount[i] < amount[i + 1]) {
-                  temp= amount[i + 1];
-                  amount[i + 1] = amount[i];
-                  amount[i] = temp;
-               }
+                for (int i = 0; i <= amount.Length - 2; i++) {
+                    if (amount[i] < amount[i + 1]) {
+                        temp = amount[i + 1];
+                        amount[i + 1] = amount[i];
+                        amount[i] = temp;
+                    }
+                }
             }
-         }
 
             for(int i=0; i<amount.Length; i++){
                 Console.Write(amount[i] + " - ");
